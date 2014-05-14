8811f0d4314ae1cf011a2326a01972afd56cb9ea
Aaron Mansheim <ajm475@drexel.edu>
2014-05-14 16:02:08 -0400

    Now installs iRODS 4.0.

--- irods.sh
+++ irods.sh
@@ -1,48 +1,48 @@
 #!/bin/bash -x
 
-IRODS_DIR=/var/lib/iRODS
-IRODS_TGZ=3.3.tar.gz
-IRODS_URL=https://github.com/irods/irods-legacy/archive/$IRODS_TGZ
+IRODS_DEB=irods-icat-4.0.0-64bit.deb
+IRODS_URL=ftp://ftp.renci.org/pub/irods/releases/4.0.0/$IRODS_DEB
+PLUGIN_DEB=irods-database-plugin-postgres-1.0.deb
+PLUGIN_URL=ftp://ftp.renci.org/pub/irods/releases/4.0.0/$PLUGIN_DEB
+IRODS_HOME=/var/lib/irods/iRODS
+IRODS_ETC=/etc/irods
 
+set -v
 
-if [ ! -e /home/vagrant/.irodsprovisioned ]; then
+if [ ! -e $IRODS_HOME/.irodsprovisioned ]; then
     apt-get update
     apt-get upgrade -y
-    apt-get install -q -y curl build-essential python-pip git python-dev postgresql odbc-postgresql unixodbc-dev language-pack-en
+    apt-get install -q -y postgresql odbc-postgresql unixodbc libssl0.9.8 super
 
-    cd /var/lib
+    cd /var/cache/apt/archives
+    [ -e $IRODS_DEB  ] || wget -q $IRODS_URL
+    [ -e $PLUGIN_DEB ] || wget -q $PLUGIN_URL
+    dpkg -i $IRODS_DEB $PLUGIN_DEB
 
-    wget -q $IRODS_URL
-    tar xzf $IRODS_TGZ
-
-    # Fix directories from tar.gz from GitHub
-    mv irods-*/iRODS .
-    rm -rf irods-*
-
-    cp /vagrant/irods.config $IRODS_DIR/config/
-
-    chown -R vagrant:vagrant $IRODS_DIR
-    chmod -R a+rx $IRODS_DIR
+    su -c 'mkdir /tmp/irods' - irods
+    su -c 'touch /tmp/irods/setup_database.flag' - irods
+    cp /vagrant/irods.config /etc/irods/irods.config
 
     # Use system-wide postgresql for iRODS
     echo "CREATE ROLE vagrant PASSWORD 'md5ce5f2d27bc6276a03b0328878c1dc0e2' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;" | su - postgres -c psql -
+    cat <<EOT | su - postgres -c psql -
+CREATE DATABASE icat;
+CREATE ROLE irods LOGIN PASSWORD 'YMLh1qJKTDbS';
+GRANT CREATE ON DATABASE icat TO irods;
+EOT
+
     # psql createuser does not allow password via cmdline
     #su postgres -c "createuser vagrant -s -w"
 
-    cp /vagrant/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
-    ln -sf /usr/lib/x86_64-linux-gnu/odbc/psqlodbca.so /usr/lib/postgresql/9.3/lib/libodbcpsql.so
     service postgresql restart
     sleep 5
 
-    su vagrant -c "cd $IRODS_DIR && export USE_LOCALHOST=1 && ./scripts/configure && make && ./scripts/finishSetup --noask"
-    RESULT=$?
+    sed -i -e 's/ \( read [^-]\)/ [ $YES ] ||\1/' ~irods/packaging/setup_database.sh
 
-    su vagrant -c "mkdir -p /home/vagrant/.irods"
+    su -c "echo 'YMLh1qJKTDbS' | YES=1 ./packaging/setup_database.sh" - irods
 
-    echo "export PATH=\$PATH:$IRODS_DIR:$IRODS_DIR/clients/icommands/bin" >> /home/vagrant/.bashrc
-    if [ "$RESULT" != "0" ]; then
-        su vagrant -c "cd $IRODS_DIR && (yes | ./irodssetup)"
-    fi
+    echo "export PATH=\$PATH:$IRODS_HOME:$IRODS_HOME/clients/icommands/bin" >> $IRODS_HOME/../.bashrc
+    chown irods:irods $IRODS_HOME/../.bashrc
 
-    touch /home/vagrant/.irodsprovisioned
+    su -c "touch $IRODS_HOME/.irodsprovisioned" - irods
 fi
